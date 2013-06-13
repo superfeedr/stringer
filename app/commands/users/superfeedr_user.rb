baaf006eba5ee5dd1935769fcdf77fe9777213d9
Pedro Sampaio <pedro@oitozero.com>
2013-06-13 10:29:35 +0100

    remove tests/db migration

--- app/commands/users/superfeedr_user.rb
+++
@@ -1,9 +0,0 @@
-class SuperfeedrUser
-  def self.complete(user)
-  	user.superfeedr_host = "localhost"
-    user.superfeedr_username = "demo"
-    user.superfeedr_password = "demo"
-    user.save
-    user
-  end
-end
--- db/migrate/20130604175937_add_pubsubhubbub_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddPubsubhubbubToUser < ActiveRecord::Migration
-  def change
-    
-    add_column :users, :pubsubhubbub_host, :string
-    add_column :users, :pubsubhubbub_username, :string
-    add_column :users, :pubsubhubbub_password, :string
-
-  end
-end
